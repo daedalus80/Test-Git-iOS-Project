fd8d1aeab00fe0757fa39eee97c7a7fc535077c9
Thomas Schäfer <thomasschaefer@McSheepPro13.local>
2014-02-25 19:48:46 +0100

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Thomas Schäfer on 25.02.14.
+//  Copyright (c) 2014 CC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Thomas Schäfer on 25.02.14.
+//  Copyright (c) 2014 CC. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
