2af3c3b7048181ae4162ab388a714d5a820581b7
Thomas Schäfer <thomasschaefer@McSheepPro13.local>
2014-02-25 19:30:35 +0100

    Initial Commit

---
+++ Git Test/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Git Test
+//
+//  Created by Thomas Schäfer on 25.02.14.
+//  Copyright (c) 2014 CC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Git Test
+//
+//  Created by Thomas Schäfer on 25.02.14.
+//  Copyright (c) 2014 CC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
